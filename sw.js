0566c79c02f0b76b28ce413a04c79cb0fac002af
ustiashviliustiashvili-cloud <ustiashviliustiashvili@gmail.com>
2026-09-11 13:39:18 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "koki-v1";
+
+const files = [
+  "./",
+  "./index.html",
+  "./koki.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(files))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
